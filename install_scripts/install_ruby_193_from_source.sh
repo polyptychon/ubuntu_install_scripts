2522ea7c791b81c3484e150c1d9a4ac80a41daa7
Haris Dimitriou <xaris.dimitriou@gmail.com>
2013-01-31 10:46:10 +0200

    Added zlib1g to the required packages for ruby

--- install_scripts/install_ruby_193_from_source.sh
+++ install_scripts/install_ruby_193_from_source.sh
@@ -5,7 +5,7 @@ set -x
 sudo apt-get update -y
 
 # install ruby pre-requisites
-sudo apt-get install build-essential zlib1g-dev libssl-dev libreadline-dev git-core curl libyaml-dev libcurl4-gnutls-dev libsqlite3-dev apache2-dev -y
+sudo apt-get install build-essential zlib1g zlib1g-dev libssl-dev libreadline-dev git-core curl libyaml-dev libcurl4-gnutls-dev libsqlite3-dev apache2-dev -y
 
 # Download , uncompress and install ruby 193
 curl --remote-name http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p327.tar.gz
