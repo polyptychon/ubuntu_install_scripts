07978b4a03614909394531e5b909d3814334f9c2
Haris Dimitriou <xaris.dimitriou@gmail.com>
2013-01-31 11:00:49 +0200

    Added installation of libyaml for ruby

--- install_scripts/install_ruby_193_from_source.sh
+++ install_scripts/install_ruby_193_from_source.sh
@@ -7,6 +7,19 @@ sudo apt-get update -y
 # install ruby pre-requisites
 sudo apt-get install build-essential zlib1g zlib1g-dev libssl-dev libreadline-dev git-core curl libyaml-dev libcurl4-gnutls-dev libsqlite3-dev apache2-dev -y
 
+#install libyaml (needed for pcych)
+wget http://pyyaml.org/download/libyaml/yaml-0.1.4.tar.gz
+tar xzf yaml-0.1.4.tar.gz
+cd yaml-0.1.4
+./configure
+make
+sudo make install
+# clean up yaml files
+cd ..
+rm yaml-0.1.4.tar.gz
+rm -rf yaml-0.1.4/
+
+
 # Download , uncompress and install ruby 193
 curl --remote-name http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p327.tar.gz
 tar zxf ruby-1.9.3-p327.tar.gz
