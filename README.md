125bca128b70072f4832bba15cb9bf99daf83fb1
Rolf Lifvergren <liffon@gmail.com>
2013-03-29 10:33:12 +0100

    Added Spotify for Linux preview install script.

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Basic install scripts for
 * Ruby 1.9.3
 * Puppet (gem)
 * Dropbox
+* Spotify for Linux preview
---
+++ install_scripts/install_spotify.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "This script requires superuser access."
+echo "Please give your password when prompted by sudo."
+
+# reset sudo permissions
+sudo -k
+
+sudo sh <<SCRIPT
+
+	# add the repository for dropbox
+	echo "deb http://repository.spotify.com stable non-free" > /etc/apt/sources.list.d/spotify.list
+
+	# add the repository key
+	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
+
+	# update package list
+	apt-get update
+
+	# install dropbox
+	apt-get install spotify-client
+
+SCRIPT
+
